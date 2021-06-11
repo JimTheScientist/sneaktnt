66532c9d7d09fa33b5c8c55133a0999338578c97
AidanP <aidannpratt@gmail.com>
2021-06-11 02:25:38 -0400

    epic cplugin

--- src/main/java/net/jimthescientist/sneaktnt/event/SneakEvent.java
+++ src/main/java/net/jimthescientist/sneaktnt/event/SneakEvent.java
@@ -3,9 +3,13 @@ package net.jimthescientist.sneaktnt.event;
 import net.jimthescientist.sneaktnt.SneakTNT;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.entity.TNTPrimed;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.text.html.parser.Entity;
 
 public class SneakEvent implements Listener {
     @EventHandler
@@ -13,7 +17,9 @@ public class SneakEvent implements Listener {
         if (SneakTNT.playing == true){
             Location playerLocation = event.getPlayer().getLocation();
             World world = event.getPlayer().getWorld();
-            world.createExplosion(playerLocation, 3);
+            //world.createExplosion(playerLocation, 5);
+            @NotNull TNTPrimed tnt = event.getPlayer().getWorld().spawn(event.getPlayer().getTargetBlock(null, 50).getLocation().add(0, 1, 0), TNTPrimed.class);
+            ((TNTPrimed)tnt).setFuseTicks(40);
         }
     }
 }
